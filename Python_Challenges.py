2776b8a0de386ee15d0f8395f43f0275bfaa35e6
Marco Andre Lostaunau <lostaunaum@gmail.com>
2014-11-24 14:59:19 -0600

    added battleship challenge to the commit

--- Python_Challenges.py
+++ Python_Challenges.py
@@ -68,4 +68,63 @@ print get_letter_grade(class_average)
 
 
 
-################## Student Grade Calculator ##################
+################## Battle-Ship Against the AI ##################
+
+from random import randint
+
+#setting up the board
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+#these will be the coordinates of the computer hidden ship
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+print "Let's play Battleship! By Marco A. Lostaunau"
+
+#game logic
+for turn in range(5):
+    #if user reaches 5 turns without successfully destroying ship user loses
+    if turn == 4:
+        print "Game Over"
+        break
+    
+    print "Turn", turn + 1 #printing the current turn
+    print_board(board)
+    #prompting the user to ask for input
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    #if user guess is correct reacting
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        break
+    else:
+        #making sure the guess is within the board
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+            turn -= 1
+            
+        #making sure the user does not guess same twice
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+            turn -= 1
+            
+        #if not correct inform user print board again and set new x for the value
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X"
+
+
+################## Battle-Ship Against the AI ##################
